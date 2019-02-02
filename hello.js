33ee8101ef27a7ce646f6c685d3fd7d19ff0eae7
Frautroll <frautroll@googlemail.com>
2019-02-02 11:42:47 +0100

    Response eingefügt

--- hello.js
+++ hello.js
@@ -1,9 +1,22 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
+app.get("/", function(req, res){
+  res.status(302).send("Root");
+})
+
+app.get("/hello", function (req, res) {
+  res.status(200).send("Hello World!");
+  //res.send("Hello World!");
 });
+
+app.get("/google", function(req, res){
+  res.status(302).send("Google");
+})
+
+app.get("/fail", function(req, res){
+  res.status(500).send("Fehler");
+})
 
 app.listen(3000, function() {
   console.log ("Lauschen auf Port 3000.");
