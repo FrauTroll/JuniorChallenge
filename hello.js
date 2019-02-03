c6cf5a1920a00be0214461d31a36f98cf6a50064
Frautroll <frautroll@googlemail.com>
2019-02-03 11:56:47 +0100

    jsHint eingefügt und angepasst. hello.js korrigiert.

--- hello.js
+++ hello.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.get("/", function(req, res){
   res.redirect("/hello");
-})
+});
 
 app.get("/hello", function (req, res) {
   res.status(200).send("Hello World!");
@@ -12,12 +12,12 @@ app.get("/hello", function (req, res) {
 app.get("/google", function(req, res){
   //Redirect auf Status 302
   res.redirect("http://www.google.de");
-})
+});
 
 app.get("/fail", function(req, res){
   res.sendStatus(500);
-})
+});
 
 app.listen(3000, function() {
   console.log ("Lauschen auf Port 3000.");
-})
+});
